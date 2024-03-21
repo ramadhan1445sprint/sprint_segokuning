fea343cc5aa2504f428391e26a7efaede260af2d
Goberman15 <akbarrmdhn94@gmail.com>
2024-03-21 12:04:13 +0700

    Update sslmode db params using env variable

--- db/conn.go
+++ db/conn.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"fmt"
+	"strings"
 
 	_ "github.com/jackc/pgx/v4/stdlib"
 	"github.com/jmoiron/sqlx"
@@ -9,20 +10,20 @@ import (
 )
 
 func NewDatabase() (*sqlx.DB, error) {
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s",
+	sslmode := config.GetString("DB_PARAMS")
+	sslmode = strings.TrimPrefix(sslmode, "&")
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s %s",
 		config.GetString("DB_HOST"),
 		config.GetString("DB_PORT"),
 		config.GetString("DB_USERNAME"),
 		config.GetString("DB_PASSWORD"),
 		config.GetString("DB_NAME"),
+		sslmode,
 	)
 
-	env := config.GetString("ENV")
-
-	if env == "production" {
-		dsn += " sslmode=verify-full rootcert=ap-southeast-1-bundle.pem"
-	} else {
-		dsn += " sslmode=disable"
+	if sslmode == "sslmode=verify-full" {
+		dsn += " rootcert=ap-southeast-1-bundle.pem"
 	}
 
 	db, err := sqlx.Connect("pgx", dsn)
