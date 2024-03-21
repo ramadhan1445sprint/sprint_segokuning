a0b477a38161f8347b140fc294a26e74c84b8304
ilhamnyto <ilhamnuryanto@student.telkomuniversity.ac.id>
2024-03-22 02:05:04 +0700

    fix: post

--- entity/post.go
+++ entity/post.go
@@ -3,42 +3,62 @@ package entity
 type Post struct {
 	ID         string   `json:"postId"`
 	UserID     string   `json:"user_id"`
-	PostInHtml   string  `json:"postInHtml" db:"post_in_html"`
+	PostInHtml string   `json:"postInHtml" db:"post_in_html"`
 	Tags       []string `json:"tags"`
 	CreatedAt  string   `json:"createdAt" db:"created_at"`
 	UpdatedAt  string   `json:"updatedAt,omitempty"`
 }
 
 type PostData struct {
-    PostId     string    `db:"post_id"`
-    PostInHtml string    `db:"post_in_html"`
-    Tags       []string  `db:"tags"`
-    CreatedAt  string `db:"post_created_at"`
-    Creator    PostUser `db:"creator"`
-    Comments   []PostComment `db:"commentx"`
+	PostId   string         `json:"postId"`
+	Post     PostDetail     `json:"post"`
+	Comments []PostComments `json:"comment"`
+	Creator  PostUser       `json:"creator"`
+}
+
+type PostDetail struct {
+	PostInHtml string      `json:"postInHtml"`
+	Tags       interface{} `json:"tags"`
+	CreatedAt  string      `json:"created_at"`
+}
+
+type PostComments struct {
+	Comment   string   `json:"comment"`
+	Creator   PostUser `json:"creator"`
+	CreatedAt string   `json:"createdAt"`
 }
 
 type PostUser struct {
-    UserId      string `db:"user_id"`
-    Name        string `db:"name"`
-    ImageUrl    string `db:"image_url"`
-    FriendCount int    `db:"friend_count"`
-    CreatedAt   string `db:"created_at"`
+	UserID      string `json:"userID"`
+	Name        string `json:"name"`
+	ImageUrl    string `json:"imageUrl"`
+	FriendCount int    `json:"friendCount"`
+	CreatedAt   string `json:"createdAt"`
 }
 
-type PostComment struct {
-    Creator   PostUser
-}
-
-
-type PostRawDBResponse struct {
+type PostRawDBData struct {
+	PostID                    string      `json:"postId" db:"post_id"`
+	PostInHTML                string      `json:"postInHtml" db:"post_in_html"`
+	Tags                      interface{} `json:"tags" db:"posts_tags"`
+	PostCreatedAt             string      `json:"postCreatedAt" db:"posts_created_at"`
+	CreatorID                 string      `json:"creatorId" db:"creator_id"`
+	CreatorName               string      `json:"creatorName" db:"creator_name"`
+	CreatorImageURL           string      `json:"creatorImageUrl" db:"creator_image_url"`
+	CreatorFriendCount        int         `json:"creatorFriendCount" db:"creator_friend_count"`
+	CreatorCreatedAt          string      `json:"creatorCreatedAt" db:"creator_created_at"`
+	Comment                   *string     `json:"comment" db:"comment,omitempty"`
+	CommentCreatedAt          *string     `json:"commentCreatedAt" db:"comment_created_at,omitempty"`
+	CommentCreatorID          *string     `json:"commentCreatorId" db:"comment_creator_id,omitempty"`
+	CommentCreatorName        *string     `json:"commentCreatorName" db:"comment_creator_name,omitempty"`
+	CommentCreatorImageUrl    *string     `json:"commentCreatorImageUrl" db:"comment_creator_image_url,omitempty"`
+	CommentCreatorFriendCount *int        `json:"commentCreatorFriendCount" db:"comment_creator_friend_count,omitempty"`
 }
 
 type PostFilter struct {
-	Limit   int  `json:"limit"`
-	Offset  int	 `json:"offset"`
-	Search  string  `json:"search"`
-	SearchTag  []string `json:"searchTag"`
+	Limit     int      `json:"limit"`
+	Offset    int      `json:"offset"`
+	Search    string   `json:"search"`
+	SearchTag []string `json:"searchTag"`
 }
 
 type Meta struct {
--- repo/post.go
+++ repo/post.go
@@ -1,6 +1,9 @@
 package repo
 
 import (
+	"encoding/json"
+	"fmt"
+	"strings"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/ramadhan1445sprint/sprint_segokuning/entity"
@@ -8,7 +11,10 @@ import (
 
 type PostRepo interface {
 	CreatePost(post *entity.Post) error
-	GetPost(userId string) ([]entity.PostData, error)
+	GetPost(filter *entity.PostFilter) ([]entity.PostData, error)
+	GetTotalPost() (int, error)
+	CheckPostById(postId string) (bool, error)
+	CheckFriendPost(postId string, userId string) (bool, error)
 }
 
 type postRepo struct {
@@ -29,43 +35,190 @@ func (r *postRepo) CreatePost(post *entity.Post) error {
 	return nil
 }
 
+func (r *postRepo) GetTotalPost() (int, error) {
+	var total int
 
-func (r *postRepo) GetPost(postId string) ([]entity.PostData, error) {
-	var posts []entity.PostData
+	err := r.db.Get(total, "SELECT count(*) from posts")
 
+	if err != nil {
+		return 0, err
+	}
+
+	return total, nil
+}
+
+func (r *postRepo) CheckPostById(postId string) (bool, error) {
+	var exist int
+
+	err := r.db.Get(exist, "SELECT count(*) from posts where id = $1", postId)
+
+	if err != nil {
+		return false, err
+	}
+
+	if exist > 0 {
+		return true, nil
+	}
+
+	return false, nil
+
+}
+func (r *postRepo) CheckFriendPost(postId string, userId string) (bool, error) {
+	var creator string
+	var exist int
+
+	err := r.db.Get(creator, "SELECT user_id from posts where id = $1", postId)
+	if err != nil {
+		return false, err
+	}
+
+	err = r.db.Get(creator, "SELECT count(*) from posts where (userid1 = $1 and userid2 = $2) or (userid1 = $3 and userid2 = $4)", userId, creator, creator, userId)
+	if err != nil {
+		return false, err
+	}
+
+	if exist > 0 {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func (r *postRepo) GetPost(filter *entity.PostFilter) ([]entity.PostData, error) {
+	where := ""
 	query := `
-		SELECT
-			p.id AS "posts.",
-			p.post_in_html AS "posts.post_in_html",
-			p.tags AS "posts.tags",
-			p.created_at AS "posts.created_at",
-			u.id AS "creator.id",
-			u.name AS "creator.name",
-			u.image_url AS "creator.image_url",
-			u.friend_count AS "creator.friend_count",
-			u.created_at AS "creator.created_at",
-			c.comment AS "comments",
-			cu.id AS "comments.creator.id",
-			cu.name AS "comments.creator.name",
-			cu.image_url AS "comments.creator.image_url",
-			cu.friend_count AS "comments.creator.friend_count",
-			cu.created_at AS "comments.creator.created_at"
-		FROM
-			posts p
-		JOIN
-			users u ON p.user_id = u.id
-		JOIN
-			comments c ON p.id = c.post_id
-		JOIN
-			users cu ON c.user_id = cu.id
-			WHERE p.id = $1
-			ORDER BY
-		p.created_at DESC, c.created_at DESC
+	SELECT
+		p.id AS "post_id",
+		p.post_in_html AS "post_in_html",
+		p.tags AS "posts_tags",
+		p.created_at AS "posts_created_at",
+		u.id AS "creator_id",
+		u.name AS "creator_name",
+		u.image_url AS "creator_image_url",
+		u.friend_count AS "creator_friend_count",
+		u.created_at AS "creator_created_at",
+		c.comment AS comment,
+		c.created_at AS "comment_created_at",
+		cu.id AS "comment_creator_id",
+		cu.name AS "comment_creator_name",
+		cu.image_url AS "comment_creator_image_url",
+		cu.friend_count AS "comment_creator_friend_count"
+	FROM
+		posts p
+	JOIN
+		users u ON p.user_id = u.id
+	left JOIN
+		comments c ON p.id = c.post_id
+	left JOIN
+		users cu ON c.user_id = cu.id
 	`
 
-	if err := r.db.QueryRowx(query, postId).Scan(&posts); err != nil {
+	if filter.Search != "" {
+		if where != "" {
+			where += " AND p.post_in_html LIKE '%" + filter.Search + "%' "
+		} else {
+			where += " WHERE p.post_in_html LIKE '%" + filter.Search + "%' "
+		}
+	}
+
+	if len(filter.SearchTag) > 0 {
+		jsonTag, err := json.Marshal([]string(filter.SearchTag))
+		if err == nil {
+			replacer := strings.NewReplacer("[", "{", "]", "}")
+			stringTag := replacer.Replace(string(jsonTag))
+			if where != "" {
+				where += fmt.Sprintf(" AND tags && '%s'", stringTag)
+			} else {
+				where += fmt.Sprintf(" WHERE tags && '%s'", stringTag)
+			}
+		}
+	}
+	query += where
+	query += fmt.Sprintf(" ORDER BY p.created_at DESC, c.created_at desc limit %d offset %d", filter.Limit, filter.Offset)
+	fmt.Print(query)
+
+	rows, err := r.db.Query(query)
+
+	if err != nil {
 		return nil, err
 	}
 
-	return posts, nil
+	postData := []entity.PostData{}
+	postMap := make(map[string]entity.PostData)
+	postMapComment := make(map[string][]entity.PostComments)
+
+	for rows.Next() {
+		var postRawDBData entity.PostRawDBData
+		var tempPostComment entity.PostComments
+		var tempPostDetail entity.PostDetail
+		var tempPostCreator entity.PostUser
+		var tempPostCommentCreator entity.PostUser
+		var tempPostData entity.PostData
+
+		if err = rows.Scan(&postRawDBData.PostID, &postRawDBData.PostInHTML, &postRawDBData.Tags, &postRawDBData.PostCreatedAt,
+			&postRawDBData.CreatorID, &postRawDBData.CreatorName, &postRawDBData.CreatorImageURL, &postRawDBData.CreatorFriendCount, &postRawDBData.CreatorCreatedAt, &postRawDBData.Comment, &postRawDBData.CommentCreatedAt,
+			&postRawDBData.CommentCreatorID, &postRawDBData.CommentCreatorName, &postRawDBData.CommentCreatorImageUrl, &postRawDBData.CommentCreatorFriendCount,
+		); err != nil {
+			return nil, err
+		}
+
+		tempPostData.PostId = postRawDBData.PostID
+		tempPostDetail.PostInHtml = postRawDBData.PostInHTML
+		tempPostDetail.Tags = postRawDBData.Tags
+		tempPostDetail.CreatedAt = postRawDBData.PostCreatedAt
+		tempPostData.Post = tempPostDetail
+
+		tempPostCreator.UserID = postRawDBData.CreatorID
+		tempPostCreator.Name = postRawDBData.CreatorName
+		tempPostCreator.ImageUrl = postRawDBData.CreatorImageURL
+		tempPostCreator.FriendCount = postRawDBData.CreatorFriendCount
+		tempPostCreator.CreatedAt = postRawDBData.CreatorCreatedAt
+		tempPostData.Creator = tempPostCreator
+
+		if _, ok := postMap[postRawDBData.PostID]; !ok {
+			tempPostData.Comments = []entity.PostComments{}
+			postMap[postRawDBData.PostID] = tempPostData
+		}
+
+		if postRawDBData.CommentCreatorID != nil {
+			tempPostCommentCreator.UserID = *postRawDBData.CommentCreatorID
+		} else {
+			continue
+		}
+
+		if postRawDBData.CommentCreatorName != nil {
+			tempPostCommentCreator.Name = *postRawDBData.CommentCreatorName
+		}
+
+		if postRawDBData.CommentCreatorImageUrl != nil {
+			tempPostCommentCreator.ImageUrl = *postRawDBData.CommentCreatorImageUrl
+		}
+		if postRawDBData.CommentCreatorFriendCount != nil {
+			tempPostCommentCreator.FriendCount = *postRawDBData.CommentCreatorFriendCount
+		}
+
+		if postRawDBData.Comment != nil {
+			tempPostComment.Comment = *postRawDBData.Comment
+		}
+
+		tempPostComment.Creator = tempPostCommentCreator
+
+		if postRawDBData.CommentCreatedAt != nil {
+			tempPostComment.CreatedAt = *postRawDBData.CommentCreatedAt
+		}
+
+		if _, ok := postMapComment[postRawDBData.PostID]; !ok {
+			postMapComment[postRawDBData.PostID] = []entity.PostComments{tempPostComment}
+		} else {
+			postMapComment[postRawDBData.PostID] = append(postMapComment[postRawDBData.PostID], tempPostComment)
+		}
+	}
+
+	for key, _ := range postMap {
+		temp := postMap[key]
+		temp.Comments = postMapComment[key]
+		postData = append(postData, temp)
+	}
+
+	return postData, nil
 }
