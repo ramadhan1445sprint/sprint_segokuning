5cbfc684a9beaf73acde1709c6b0df256c057524
Goberman15 <akbarrmdhn94@gmail.com>
2024-03-23 14:11:58 +0700

    Change JWT expire time

--- crypto/jwt.go
+++ crypto/jwt.go
@@ -15,7 +15,7 @@ func GenerateToken(id, name string) (string, error) {
 		Id:              id,
 		Name:            name,
 		RegisteredClaims: jwt.RegisteredClaims{
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(10 * time.Minute)),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(2 * time.Minute)),
 		},
 	})
 
